a39d30e311b121c347071cc6b4b9fd6b3cc4064c
‘penjoe’ <pennockjoe@gmail.com>
2020-04-16 00:09:21 -0700

    refactored weather data to pull from API

--- server.js
+++ server.js
@@ -10,29 +10,15 @@ const cors = require('cors');
 // Taking PORT from .env
 const PORT = process.env.PORT || 3000;
 
-function Location(city, data) {
-  this.search_query = city;
-  this.formatted_query = data.display_name;
-  // this.latitude = data.lat;
-  // this.longitude = data.lon;
-}
-
-function Forecast(date, forecast) {
-  this.forecast = forecast;
-  this.time = new Date(date).toDateString();
-}
-
 //request comes from front end via user input, response is data server sends back
 function handleLocation( request, response) {
   let cityQuery = request.query.city; // input from user
-  const key = process.env.LOCATIONIQ_API_KEY; // api key
-  const locationURL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityQuery}&format=json&limit=1` // location API url
-
+  const locationKey = process.env.LOCATIONIQ_API_KEY; // api key
+  const locationURL = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${cityQuery}&format=json&limit=1` // location API url
   superagent.get(locationURL)
     .then( locationResponse => {
       const data = locationResponse.body;
       data.map( idx => {
-        console.log('.map parameter', idx)
         if (idx.display_name.search(cityQuery)) {
           const location = new Location(cityQuery, idx)
           response.send(location);
@@ -44,22 +30,43 @@ function handleLocation( request, response) {
     });
 }
 
-// handleError('something is wrong', request, response)
-
-
-function handleWeather(request, response) {
-  const weatherData = require('./data/darksky.json').daily.data;
-
-  const results = [];
-  weatherData.map(item => results.push(new Forecast(item.time, item.summary)))
-  response.send(results);
+// creates location objects
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
 }
 
+// handleError('something is wrong', request, response)
+// Gets location data
+function handleWeather(request, response) {
+  let {latitude, longitude} = request.query;
+  const weatherKey = process.env.WEATHERBIT_API_KEY;
+  const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherKey}&lang=en&units=I&days=8&lat=${latitude}&lon=${longitude}`;
+  
+  superagent.get(weatherUrl)
+    .then(value => {
+      const weather = value.body.data;
+      
+      response.send(weather.map( idx => {
+        return new Forecast(idx.datetime, idx.weather.description)
+      }));
+    })
+    .catch(error => {
+      handleError('weather error', request, response, next)
+    });
+}
+
+// creates weather objects
+function Forecast(date, forecast) {
+  this.forecast = forecast;
+  this.time = new Date(date).toDateString();
+}
+
+// Gets weather data
 function handleError(error, request, response, next) {
-  response.status(500).send({
-    status: 500,
-    responseTest: 'sorry, something went wrong',
-  });
+  response.status(500).send(error);
 }
 
 // using express to call
